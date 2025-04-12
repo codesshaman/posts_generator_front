07ed92bfb0187386eeedc2b445d88ee3d6a8b9ef
codesshaman <nicksemail@yandex.ru>
2025-04-12 21:01:46 +0300

    last changes

--- apps/posting/views_settings.py
+++ apps/posting/views_settings.py
@@ -1,11 +1,8 @@
-from project.language import translate
+from project.language import translate, language
 from django.shortcuts import render
 from dotenv import load_dotenv
 from datetime import datetime
 import os
-
-# Устанавливаем язык
-lang = "en"
 
 # Загружаем .env файл
 load_dotenv()
@@ -18,13 +15,10 @@ def settings(request):
     """Отображает страницу настроек"""
     if debug:
         print("Отображаем страницу настроек")
+    lang = language(request)
     return render(request, "posting/settings.html", {
         # "posts": initial_posts,
         # "has_more": has_more,
         "title": translate("Настройки", lang),
         "h2_text": translate("Настройки", lang),
-        "publish_text": translate("Публикация: ", lang),
-        "read_button": translate("Читать статью", lang),
-        "load_more_text": translate("Загрузить ещё", lang),
-        "no_more_posts_text": translate("Больше постов нет", lang),
     })
