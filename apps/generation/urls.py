28ac7d8563732cb2d6d5e863153727f8bf87eed5
codesshaman <nicksemail@yandex.ru>
2025-06-27 22:45:45 +0300

    add random analize timer to step 1

--- apps/generation/urls.py
+++ apps/generation/urls.py
@@ -1,4 +1,4 @@
-from .step_1 import process_group_selection
+from .step_1 import process_group_selection, check_analysis_status
 from .step_2 import get_group_topics
 from .step_3 import receive_content_plan
 from .step_4 import posts_generation
@@ -10,6 +10,7 @@ from django.urls import path
 urlpatterns = [
     path('autogeneration', autogeneration, name='autogeneration'),
     path('process-group-selection/', process_group_selection, name='process_group_selection'),
+    path('check-analysis-status/', check_analysis_status, name='check_analysis_status'),
     path('get-group-topics/', get_group_topics, name='get_group_topics'),
     path('receive-content-plan/', receive_content_plan, name='receive_content_plan'),
     path('generate-content-plan/', posts_generation, name='generate_content_plan'),
