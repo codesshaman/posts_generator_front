f8a741ccf7b7c6f82d752549e44c5da3c652083e
codesshaman <nicksemail@yandex.ru>
2025-08-08 22:26:45 +0300

    create post page changes

--- apps/createpost/urls.py
+++ apps/createpost/urls.py
@@ -1,3 +1,4 @@
+from .generate_content import generate_content_api
 from django.urls import path
 from .views import *
 
@@ -5,4 +6,5 @@ from .views import *
 urlpatterns = [
     path('create_post', create_post, name='create_post'),
     path('get_next_id/', generate_unique_id, name='generate_id'),
+    path('generate_one_post/', generate_content_api, name='generate_content_api')
 ]
